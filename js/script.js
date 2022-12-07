8acbb7b78f63bb1105da81e471d98cf3a65a0431
Ah9797 <89481488+Ah9797@users.noreply.github.com>
2022-12-06 19:49:44 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function initMap() {
   var maptest = {
-	center:new google.maps.LatLng(41.054,-87.512),
+	center: new google.maps.LatLng(41.054,-87.512),
 	zoom: 8,
   };
  var map = new google.maps.Map(document.getElementById("map"),maptest);
